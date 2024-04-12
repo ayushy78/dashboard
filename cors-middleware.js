218486b0e5e5ff0583d6eb5cc4bbb8748b031473
Aman Gupta <amanguptaprogrammer@gmail.com>
2024-04-12 14:32:56 +0530

    mid

--- cors-middleware.js
+++ cors-middleware.js
@@ -1,9 +1,9 @@
-const jsonServer = require('json-server')
-    
-// Import the library:
-const cors = require('cors');
+const jsonServer = require("json-server");
 
-const server = jsonServer.create()       
-        
+// Import the library:
+const cors = require("cors");
+
+const server = jsonServer.create();
+
 // Then use it before your routes are set up:
 server.use(cors());
